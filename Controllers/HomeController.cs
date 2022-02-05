d58be70d8571a76b767aecdffd80a3c71243f11c
Furqat-DevDo <furqata93@gmail.com>
2022-02-06 01:10:58 +0500

    FireBase Added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,22 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FireBaseMVC.Models;
+using FireSharp.Interfaces;
+using FireSharp.Config;
+using FireSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace FireBaseMVC.Controllers;
 
 public class HomeController : Controller
 {
+    IFirebaseConfig config = new FirebaseConfig
+    {
+        AuthSecret = "YFzrUL9OO2iORvZgyynkYsrh4QE7SqrqoOv6yQpe",
+        BasePath = "https://capable-boulder-323806-default-rtdb.europe-west1.firebasedatabase.app/"
+    };
+    IFirebaseClient client;
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
@@ -16,10 +27,25 @@ public class HomeController : Controller
     [HttpGet]
     public IActionResult Index()
     {
-        return View();
+        client = new FirebaseClient(config);
+        var response = client.Get("UserModel");
+        dynamic JsonResult = JsonConvert.DeserializeObject(response.Body);
+        var list = new List<UserModel>();
+        foreach (var item in JsonResult)
+        {
+            list.Add(JsonConvert.DeserializeObject<UserModel>(((JProperty)item).Value.ToString()));
+        }
+        return View(list);
     }
     [HttpPost]
-    public IActionResult Index([FromForm] UserModel model)
+    public IActionResult Create([FromForm] UserModel model)
+    {
+        var client=new FirebaseClient(config);
+        client.Push("UserModel/", model);
+        return RedirectToAction("Index");
+    }
+    [HttpGet]
+    public IActionResult Create()
     {
         return View();
     }
